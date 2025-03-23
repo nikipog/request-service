7f1c012a11ba1fbf028234a07c623f0fa1d55414
Никита Погорелов <nikipog@mail.ru>
2025-03-23 20:46:26 +0300

    feat: добавит боковую панель

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,14 @@
-// import Sidebar from './Sidebar';
-
+import './layout.scss';
 import Header from "../header/header";
+import Sidebar from "../sidebar/sidebar";
 
 function Layout() {
   return (
     <div className="layout">
-      {/* <Sidebar /> */}
-      <div className="layout__headerContainer">
+      <Sidebar />
+      <div className="layout__main">
         <Header />
-        <main className="layout__mainContent">
+        <main className="layout__content">
 
         </main>
       </div>
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,8 +1,50 @@
+import './sidebar.scss';
+import knowledgeIcon from '../../assets/icons/knowledge-base-icon.png';
+import requestsIcon from '../../assets/icons/requests-icon.png';
+import employeesIcon from '../../assets/icons/employees.png';
+import clientsIcon from '../../assets/icons/clients-icon.png';
+import assetsIcon from '../../assets/icons/assets-icon.png';
+import settingsIcon from '../../assets/icons/settings-icon.png';
+
+
+const sidebarItems = [
+  { label: 'База знаний', icon: knowledgeIcon },
+  { label: 'Заявки', icon: requestsIcon },
+  { label: 'Сотрудники', icon: employeesIcon },
+  { label: 'Клиенты', icon: clientsIcon },
+  { label: 'Активы', icon: assetsIcon },
+  { label: 'Настройки', icon: settingsIcon },
+];
+
 function Sidebar() {
   return (
-    <nav className="sidebar">
-      <div>1</div>
-    </nav>
+    <aside className="sidebar">
+      <div className="sidebar__logo">
+        <img src="/src/assets/icons/logo-icon.png" alt='logo-icon' />
+      </div>
+      <nav className="sidebar__nav">
+        <ul className="sidebar__list">
+          {sidebarItems.map((item) => (
+
+            <li
+              className="sidebar__item"
+              key={item.label}
+            >
+              <button className="sidebar__button">
+                <img
+                  className="sidebar__icon"
+                  src={item.icon}
+                  alt={item.label}
+                  aria-hidden="true"
+                />
+                <span className="sidebar__label">{item.label}</span>
+              </button>
+            </li>
+          ))}
+
+        </ul>
+      </nav>
+    </aside>
   );
 }
 
